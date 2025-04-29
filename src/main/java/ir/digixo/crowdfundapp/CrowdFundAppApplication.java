c169242f52655d62fe2a8a56b6d3b0e398657374
efsam <E.salimi75@ymail.com>
2025-04-29 16:53:23 +0330

    project setup and basic configuration

---
+++ src/main/java/ir/digixo/crowdfundapp/CrowdFundAppApplication.java
@@ -0,0 +1,13 @@
+package ir.digixo.crowdfundapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowdFundAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrowdFundAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/digixo/crowdfundapp/CrowdFundAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.digixo.crowdfundapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrowdFundAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
