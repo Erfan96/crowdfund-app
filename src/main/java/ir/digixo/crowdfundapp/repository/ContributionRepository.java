2e6ba6597fff1a23d92faaada3eec819bf24f138
efsam <E.salimi75@ymail.com>
2025-06-06 19:53:44 +0330

    add some methods ContributionRepository

--- src/main/java/ir/digixo/crowdfundapp/repository/ContributionRepository.java
+++ src/main/java/ir/digixo/crowdfundapp/repository/ContributionRepository.java
@@ -1,15 +1,24 @@
 package ir.digixo.crowdfundapp.repository;
 
 import ir.digixo.crowdfundapp.entity.Contribution;
-import ir.digixo.crowdfundapp.entity.Project;
-import ir.digixo.crowdfundapp.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Repository
 public interface ContributionRepository extends JpaRepository<Contribution, Long> {
-    List<Contribution> findByUser(User user);
-    List<Contribution> findByProject(Project project);
+    List<Contribution> findByUserId(Long userId);
+    List<Contribution> findByProjectId(Long projectId);
+
+    @Query("select sum(c.amount) from Contribution c where c.project.id = :projectId")
+    BigDecimal getTotalAmountForProject(@Param("projectId") Long projectId);
+
+    @Query("select sum(c.amount) from Contribution c where c.user.id = :userId")
+    BigDecimal getTotalAmountForUser(@Param("userId") Long userId);
+
+    List<Contribution> findTop10ByProjectIdOrderByContributionDateDesc(Long projectId);
 }
