f6f1230a1fad813851f1540e6cadcd9f31fece71
efsam <E.salimi75@ymail.com>
2025-07-13 19:12:58 +0330

    Change in update method

--- src/main/java/ir/digixo/crowdfundapp/service/impl/UserServiceImpl.java
+++ src/main/java/ir/digixo/crowdfundapp/service/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import ir.digixo.crowdfundapp.entity.User;
 import ir.digixo.crowdfundapp.mapper.UserMapper;
 import ir.digixo.crowdfundapp.repository.UserRepository;
 import ir.digixo.crowdfundapp.service.UserService;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -45,7 +46,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDto updateUser(UserDto userDto) {
-        User user = findByUsername(userDto.getUsername());
+        User user = userRepository.findById(userDto.getId()).orElseThrow(() -> new EntityNotFoundException("User not found"));
         userMapper.updateUserFromDto(userDto, user);
         User userUpdated = userRepository.save(user);
         return userMapper.toDto(userUpdated);
